10ac9d27de4e9214c892a695ca8322d42f0355aa
rafiqulislam2000 <1520616@iub.edu.bd>
2017-06-07 01:49:39 +0600

    Assignment-7(updated)

--- smartForm.js
+++ smartForm.js
@@ -1,8 +1,8 @@
 // Your jQuery goes here
 
 var userData = {
-	name : "rafiqul",
-	email : "1520616@iub.edu.bd",
+	name : "",
+	email : "",
 	html: {likes : [], dislikes:[]},
 	css: {likes : [], dislikes:[]},
 	js: {likes : [], dislikes:[]},
@@ -13,26 +13,267 @@ $("#startBtn").click(function(event){
 	//console.log("start clicked")
 	$("#welcome").hide();
 	$("#q1").show();
+	$("#nextBtn").hide();
 });
 
 $("#name").change(function(event){
-	console.log($("#name").val());
+	$("#email").change(function(event){	
+		//console.log($("#name").val());
+		//console.log($("#email").val());
+   		 //var x = document.input["email"]["email"].value;
+   		 //var atpos = x.indexOf("@");
+    	 //var dotpos = x.lastIndexOf(".");
+    	 //if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        //	alert("Not a valid e-mail address");
+		//}
+		//var valid = true;
+		//if(valid==true){
+			$("#nextBtn").show();	
+	//	}
+});
+
+	
 	if($("#name").val()){
-		userData.name =  $("#name").val();
+		//userData.name =  $("#name").val();
 	}	
 });
 
 
-$("#email").change(function(event){
+
+$("#nextBtn").click(function(event){
 	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").show();
+});
+//2a-----------------------------------------------
+$("#htmlNxt").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2a").show();
+	$("#2aNext").hide();
+
+});
+
+//HTML checkbox
+
+$("#2a1").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a2").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a3").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a4").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a5").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a6").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a7").click(function(event){
+	$("#2aNext").show();
+});
+$("#2a8").click(function(event){
+	$("#2aNext").show();
+});
+
+$("#2aNext").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2a").hide();
+	$("#q3").show();
+
+});
+
+$("#2aPrevious").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").show();
+	$("#q2a").hide();
+	$("#q3").hide();
+
+});
+
+//2b---------------------------------------------------
+$("#cssNxt").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2b").show();
+	$("#2bNext").hide();
+	
+});
+//css checkbox
+$("#2b1").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b2").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b3").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b4").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b5").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b6").click(function(event){
+	$("#2bNext").show();
+});
+$("#2b7").click(function(event){
+	$("#2bNext").show();
+});
+
+$("#2bNext").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2b").hide();
+	$("#q3").show();
+
+});
+
+$("#2bPrevious").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").show();
+	$("#q2b").hide();
+	$("#q3").hide();
+
+});
+
+//2c----------------------------------------------------
+$("#javaScriptNxt").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2c").show();
+	$("#2cNext").hide();
 	
 });
 
+//JavaScript checkbox
+$("#2c1").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c2").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c3").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c4").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c5").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c6").click(function(event){
+	$("#2cNext").show();
+});
+$("#2c7").click(function(event){
+	$("#2cNext").show();
+});
 
-//$("#name").click(function(event){
+$("#2cNext").click(function(event){
 	//console.log("start clicked")
-//	$("#welcome").hide();
-	//$("#q1").hide();
-	//$("#q2").show();
-//});
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2c").hide();
+	$("#q3").show();
+
+});
+
+$("#2cPrevious").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").show();
+	$("#q2c").hide();
+	$("#q3").hide();
+
+});
+
+//-----------------------------------
+
+$("#q3Next").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").hide();
+	$("#q2a").hide();
+	$("#q3").hide();
+	$("#thanks").show();
+
+});
+
+$("#q3Previous").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").hide();
+	$("#q1").hide();
+	$("#nextBtn").hide();
+	$("#q2").show();
+	$("#q2a").hide();
+	$("#q3").hide();
+
+
+});
+
+//ans
+
+$("#answer").click(function(event){
+	//console.log("start clicked")
+	$("#welcome").show();
+	$("#q1").show();
+	$("#nextBtn").hide();
+	$("#htmlNxt").hide();
+	$("#2aNext").hide();
+	$("#2bNext").hide();
+	$("#2cNext").hide();
+	$("#q3Next").hide();
+	$("#cssNxt").hide();
+	$("#javaScriptNxt").hide();
+	$("#answer").hide();
+	$("#deleteAnswer").hide();
+	
+	$("#2aPrevious").hide();
+	$("#2bPrevious").hide();
+	$("#2cPrevious").hide();
+	$("#q3Previous").hide();
+	$("#q2").show();
+	$("#q2a").show();
+	$("#q2b").show();
+	$("#q2c").show();
+	$("#q3").show();
+	$("#thanks").show();
+
+});
 
